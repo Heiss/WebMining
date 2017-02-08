c2dfc866433801200f8ea6c0cd3b7c4c61388f61
Peter Heiss <peter.heiss91@gmail.com>
2017-02-08 23:22:02 +0100

    added new file for managing database

---
+++ manage.py
@@ -0,0 +1,14 @@
+# TODO: Manager for database
+# function to add and remove sites, so we dont need a sites file in directory...
+# from sys import argv
+
+
+class Manager:
+    def __init__(self):
+        pass
+
+    def addFeed(self, site):
+        pass
+
+    def removeFeed(self, site):
+        pass
