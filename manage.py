ec93e51d38ea7adc54c8a597d2857b52322b2408
Peter Heiss <peter.heiss91@gmail.com>
2017-02-19 02:23:01 +0100

    added some methods for later implementation

--- manage.py
+++ manage.py
@@ -1,10 +1,17 @@
 # TODO: Manager for database
 # function to add and remove sites, so we dont need a sites file in directory...
 # from sys import argv
+# import migrate.versioning.api
 
 
 class Manager:
     def __init__(self):
+        pass
+
+    def __init__(self, option, path):
+        pass
+
+    def __init__(self, option, path, feed):
         pass
 
     def addFeed(self, site):
